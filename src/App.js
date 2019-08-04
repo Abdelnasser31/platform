b380279c41244806e49b2aad1c24f58f2b82ea31
Mhd Bwedany <mbwedany@gmail.com>
2019-08-04 15:25:48 +0300

    filters

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Box from '@material-ui/core/Box'
+import Selectors from './Components/Selectors';
+import RadioSelectors from './Components/RadioSelectors';
+import TitleInput from './Components/TitleInput';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <TitleInput></TitleInput>
+     <Selectors></Selectors>
+     <RadioSelectors></RadioSelectors>
     </div>
   );
 }
