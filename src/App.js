4eefe0b4d0fb0c070dad0c28279d573506350c5a
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-08-07 19:29:54 +0300

    add icons to the footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Components/Header';
+import Footer from './Components/Footer/Footer';
 
 import './App.css';
 
@@ -7,6 +8,7 @@ function App() {
   return (
       <div>
         <Header></Header>
+        <Footer></Footer>
       </div>
   );
 }
