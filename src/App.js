57891788d88d9d40074c14d850cee7769e592595
Mhd Bwedany <mbwedany@gmail.com>
2019-08-08 17:05:05 +0300

    all content control

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import Box from '@material-ui/core/Box'
 import Selectors from './Components/Selectors';
 import RadioSelectors from './Components/RadioSelectors';
 import TitleInput from './Components/TitleInput';
+import RichText from './Components/RichText';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
   return (
     <div>
-     <TitleInput></TitleInput>
-     <Selectors></Selectors>
-     <RadioSelectors></RadioSelectors>
+      <Grid container spacing={3}>
+        <Grid item xs={4}>
+          <TitleInput></TitleInput>
+          </Grid>
+          <Grid item xs={4}>
+          <Selectors></Selectors>
+          </Grid>
+          <Grid item xs={4}>
+          <RadioSelectors></RadioSelectors>
+        </Grid>
+     </Grid>
+     <RichText></RichText>
     </div>
   );
 }
