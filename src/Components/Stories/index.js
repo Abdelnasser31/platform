a0d353562c3c00486a10437f41e736c8cfd25d4d
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-08-09 13:51:34 +0300

    add the stories with favorite couns and views

---
+++ src/Components/Stories/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Story from '../Story'
+import Grid from '@material-ui/core/Grid'
+
+export default function Stories({ stories, updateFavoritesCount}) {
+    return (
+        <Grid container spacing={3} justify="center">
+          {
+              stories.map(story => (
+                  <Grid item>
+                      <Story
+                      name= {story.name}
+                      storyTitle = {story.fields.title.stringValue}
+                      storyImage = {story.fields.imageURL.stringValue}
+                      storyDate = {story.createTime}
+                      favoritesCount = {story.fields.favouriteCount.integerValue}
+                      updateFavoritesCount = {updateFavoritesCount}
+                      viewsCount = {story.fields.viewCount.integerValue}
+                      ></Story>
+                     
+                      </Grid>
+              ))
+          }
+        </Grid>
+    )
+}
