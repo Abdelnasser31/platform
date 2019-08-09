5e20adec40c077b19c84f383dec935c1f0d9ed8a
Mhd Bwedany <mbwedany@gmail.com>
2019-08-09 18:31:17 +0300

    refactor adding story

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -11,6 +11,41 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+export async function postData() {
+  const storyTitle = document.getElementById('outlined-name').value;
+  const storyLocation = document.getElementById('outlined-location-native-simple').value;
+  const storyType = document.getElementById('outlined-type-native-simple').value;
+  const url = `https://firestore.googleapis.com/v1/projects/syrian-success-story/databases/(default)/documents/stories`
+  await fetch(url, {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      fields: {
+        type : {
+          stringValue: 'Syria',
+        },
+        title: {
+         
+            stringValue: 'story three'
+          
+        },
+        imageURL: {
+          stringValue: 'https://static.wixstatic.com/media/600c78_f8fe5295769848c3aae681bbfcc21d8b~mv2.jpg/v1/fill/w_740,h_346,al_c,q_90,usm_0.66_1.00_0.01/600c78_f8fe5295769848c3aae681bbfcc21d8b~mv2.jpg'
+        },
+        favouriteCount: {
+          integerValue: 0,
+        },
+        viewCount: {
+          integerValue: 0,
+        }
+      }
+    })
+  })
+  alert("done")
+}
+
 function Buttons() {
   const classes = useStyles();
 
