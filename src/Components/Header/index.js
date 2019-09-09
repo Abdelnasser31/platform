1fe65899d009c67118e66dcdea0c130e9ca1fee7
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-09-09 21:02:09 +0300

    styling the buttons

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button'
 import styles from './styles.module.css'
 import {Link} from 'react-router-dom'
@@ -20,72 +16,46 @@ function a11yProps(index: any) {
   };
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-      width: '100%',
-      marginBottom: 24,
-      
-      backgroundColor: theme.palette.background.paper,
-    },
-    tabs: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-    },
-    img: {
-        width: '80px',
-        height: '60px',
-        background: 'url("https://static.wixstatic.com/media/600c78_618e8fc2e4334570aa49d2825fd9de13~mv2_d_2700_1762_s_2.png/v1/fill/w_106,h_70,al_c,q_80,usm_0.66_1.00_0.01/SSS_En-02.webp")',
-        backgroundSize: '80px',
-        backgroundRepeat: 'no-repeat'
-    },
-    button: {
-        minHeight: '62px', 
-    },
-
-  }),
-);
-
   
 
 export default function ScrollableTabsButtonAuto() {
-  const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default" className = {classes.tabs}>
-          <div className = {classes.img}>
+    <div className={styles.header}>
+      <div position="static" color="default" className = {styles.bar}>
+          <div className = {styles.brand}>
           
          </div>
+
+
           <div>
        
               
-         <Link to="/" className={styles.link} > <Button variant="contained" href="#contained-buttons" className={classes.button}>
+         <Link to="/" className={styles.link} > <Button className={styles.button}>
         Recent Stories
       </Button></Link>
       
           <Link to="/stories" className={styles.link} >
-          <Button variant="contained" href="#contained-buttons" className={classes.button}>
+          <Button className={styles.button}>
         All Stories
       </Button>
            </Link>
 
           <Link to="/share-story" className={styles.link} >
-          <Button variant="contained" href="#contained-buttons" className={classes.button}>
+          <Button className={styles.button}>
         Share Story
       </Button>
             </Link>
           <Link to="/about" className={styles.link} >
-          <Button variant="contained" href="#contained-buttons" className={classes.button}>
+          <Button className={styles.button}>
        About
       </Button>
             
             </Link>
           <Link to="/add-story" className={styles.link} >
-          <Button variant="contained" href="#contained-buttons" className={classes.button}>
+          <Button className={styles.button}>
         Add Story
       </Button>
             </Link>
@@ -93,7 +63,7 @@ export default function ScrollableTabsButtonAuto() {
     
       
         </div>
-      </AppBar>
+      </div>
    
     </div>
   );
