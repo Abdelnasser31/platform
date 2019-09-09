4a3dd7f50f2d6f930a496bd44d4e5efe02e14edf
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-09-09 21:17:54 +0300

    DRY the navbar links using array and map

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,68 +3,34 @@ import Button from '@material-ui/core/Button'
 import styles from './styles.module.css'
 import {Link} from 'react-router-dom'
 
-
-
-const BASE_URL = 'https://firestore.googleapis.com/v1'
-
-
-
-function a11yProps(index: any) {
-  return {
-    id: `scrollable-auto-tab-${index}`,
-    'aria-controls': `scrollable-auto-tabpanel-${index}`,
-  };
-}
-
-  
+let tabs = [{to: '', name: 'Recent Stories'},
+{to: 'stories', name: 'All Stories'},
+{to: 'share-story', name: 'Share Stories'},
+{to: 'about', name: 'About'},
+{to: 'add-story', name: 'Add a Story'}];
 
 export default function ScrollableTabsButtonAuto() {
-  const [value, setValue] = React.useState(0);
-
-
   return (
     <div className={styles.header}>
-      <div position="static" color="default" className = {styles.bar}>
-          <div className = {styles.brand}>
-          
-         </div>
-
-
-          <div>
-       
+      <div position="static" color="default" className={styles.bar}>
+        <div className={styles.brand}></div>
+        <div>
+          {/* <Link to="/" className={styles.link}>
+            <Button className={styles.button}>
+              Recent Stories
+            </Button>
+          </Link> */}
+          {tabs.map(link => 
+              <Link to={`/${link.to}`} className={styles.link}>
+                <Button className={styles.button}>
+                  {link.name}
+                </Button>
+              </Link>
               
-         <Link to="/" className={styles.link} > <Button className={styles.button}>
-        Recent Stories
-      </Button></Link>
-      
-          <Link to="/stories" className={styles.link} >
-          <Button className={styles.button}>
-        All Stories
-      </Button>
-           </Link>
-
-          <Link to="/share-story" className={styles.link} >
-          <Button className={styles.button}>
-        Share Story
-      </Button>
-            </Link>
-          <Link to="/about" className={styles.link} >
-          <Button className={styles.button}>
-       About
-      </Button>
-            
-            </Link>
-          <Link to="/add-story" className={styles.link} >
-          <Button className={styles.button}>
-        Add Story
-      </Button>
-            </Link>
-       
-    
-      
+          )}
         </div>
       </div>
-   
+
     </div>
   );
 }
