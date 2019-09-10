5d2af3a115de5839a14077447b455a3b4ace3092
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-09-10 22:25:28 +0300

    add sign in component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './Components/Footer/Footer';
 import RecentStories from './Containers/RecentStories'
 import About from './Components/About'
 import StoryForm from './Components/StoryForm'
+import SignIn from './Components/SignIn'
 import StoriesList from './Containers/storiesList'
 import StoryPage from './Components/StoryPage'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
@@ -20,8 +21,9 @@ function App() {
           <Route path="/about" component={About}/>
           <Route path="/share-story" component={ShareStory}/>
           <Route path="/stories" component={StoriesList}/>
-          <Route path="/add-story" component={StoryForm}/>
+          {/* <Route path="/admin-panel" component={StoryForm}/> */}
           <Route path="/story/:id" component={StoryPage} />
+          <Route path="/admin-panel" component={SignIn} />
         </Switch>
         <Footer></Footer>
       </div>
