a5bb1d90476742372e922c81d274446ed4649c73
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-09-11 21:39:38 +0300

    fix bugs

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,22 @@ export const socialLinks = [
     { text:'LinkedIn', href: 'https://www.linkedin.com/company/syrian-success-story' },
     { text: 'Facebook', href: 'https://www.facebook.com/SyrianSuccessStory' }
   ]
+  
+  export const tabs = [
+    {
+      to: '',
+      name: 'Recent Stories'
+    }, {
+      to: 'stories',
+      name: 'All Stories'
+    }, {
+      to: 'share-story',
+      name: 'Share Stories'
+    }, {
+      to: 'about',
+      name: 'About'
+    }, {
+      to: 'add-story',
+      name: 'Add a Story'
+    }
+  ];
