89d81293680c83bdb3b5a057a127c9667f46a9a1
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-09-25 23:15:42 +0300

    make the banner responsive

--- src/Components/HomeBanner/index.js
+++ src/Components/HomeBanner/index.js
@@ -2,16 +2,19 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import styles from './styles.module.css'
-
+import Hidden from '@material-ui/core/Hidden';
 export default function homeBanner() {
 
   return (
     <div className={styles.banner}>
       
         <div className={styles.overlay}>
-        <Grid container direction="row" justify="center" alignItems="center">
-          <Typography variant="h2"><strong>WHAT IS STANDING<br/> BETWEEN <br/> YOU AND SUCCESS?</strong></Typography>
-          </Grid>
+          <Hidden xsDown>        <Grid container direction="row" justify="center" alignItems="center">
+      <Typography variant="h2"><strong>WHAT IS STANDING<br/> BETWEEN <br/> YOU AND SUCCESS?</strong></Typography>
+          </Grid></Hidden>
+          <Hidden smUp>        <Grid container direction="row" justify="center" alignItems="center">
+      <Typography variant="h4"><strong>WHAT IS STANDING<br/> BETWEEN <br/> YOU AND SUCCESS?</strong></Typography>
+          </Grid></Hidden>
         </div>
 
       
