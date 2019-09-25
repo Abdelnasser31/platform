2526936e2e5a0360110694af785d0f962f7c104a
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-09-25 23:07:22 +0300

    fix the navbar

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -8,16 +8,16 @@ import  {Navbar, Nav, Container} from 'react-bootstrap'
 export default function ScrollableTabsButtonAuto() {
   return (
     
-        <Navbar bg="light" expand="lg" className={'center'}>
+        <Navbar bg="light" expand="lg" className={`${styles.nav} p-0`}>
       <Container>
         <Navbar.Brand className={styles.brand}></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" >
             <Nav className='ml-auto'>
           {tabs.map(link => 
-            <Nav.Link className={styles.button}>
-             <Link to={`/${link.to}`}> {link.name} </Link>
-            </Nav.Link>
+            
+             <Link className={styles.link} to={`/${link.to}`}> {link.name} </Link>
+         
           )}
           </Nav>
         </Navbar.Collapse>
