ec6e739606f7e19ebb4a13e07ad7a43492c1e04e
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-09-28 11:20:33 +0300

    add filter search

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,28 +2,25 @@ import React from 'react';
 import styles from './styles.module.css'
 import {Link} from 'react-router-dom'
 import {tabs} from './../../constants'
-import  {Navbar, Nav, Container} from 'react-bootstrap'
-
+import {Navbar, Nav, Container,Form, FormControl, Button} from 'react-bootstrap'
 
 export default function ScrollableTabsButtonAuto() {
   return (
-    
-        <Navbar bg="light" expand="lg" className={`${styles.nav} p-0`}>
+
+    <Navbar bg="light" expand="lg" className={`${styles.nav} p-0`}>
       <Container>
         <Navbar.Brand className={styles.brand}></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" >
-            <Nav className='ml-auto'>
-          {tabs.map(link => 
-            
-             <Link className={styles.link} to={`/${link.to}`}> {link.name} </Link>
-         
-          )}
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className='ml-auto'>
+            {tabs.map(link => <Link className={styles.link} to={`/${link.to}`}>
+              {link.name}
+            </Link>)}
           </Nav>
+         
         </Navbar.Collapse>
-        </Container>
+      </Container>
     </Navbar>
-  
-  
+
   );
 }
--- src/Containers/storiesList/index.js
+++ src/Containers/storiesList/index.js
@@ -1,11 +1,17 @@
 import React, {useState, useEffect} from "react"
 import StoriesComponent from "../../Components/Stories"
+import {Grid} from '@material-ui/core'
+import TitleInput from '../../Components/TitleInput'
 import Spinner from '../../Components/Spinner'
+import {Button, Form, FormControl} from 'react-bootstrap'
+import {inputs} from '../../constants'
 const firebase = require('firebase');
 require('firebase/firestore');
 
 firebase.initializeApp({apiKey: ' AIzaSyBOrbITN0d8aCYV0hSZTnuilXRfDx2f9_Q ', authDomain: 'https://syrian-success-story.firebaseapp.com/', projectId: 'syrian-success-story'})
 let db = firebase.firestore();
+let stories = db.collection('stories');
+
 export default class StoriesList extends React.Component {
   state = {
     stories: null
@@ -13,24 +19,45 @@ export default class StoriesList extends React.Component {
   componentDidMount() {
     this.fetchStories();
   }
-  fetchStories = async() => {
+  fetchStories = async(text) => {
+    
     let response = []
-
+    let query;
+    if(text){
+      query = stories
+      .where('owner', '==', text)
+      .get();
+    }else{
+      query = stories
+      .orderBy('createTime', 'desc')
+      .get();
+    }
     if (this.props.number) {
       response = await db
         .collection('stories')
         .orderBy('createTime', 'desc')
         .limit(Number(this.props.number))
         .get();
+
     } else {
-      response = await db
-        .collection('stories')
-        .orderBy('createTime', 'desc')
-        .get();
+      console.log(text);
+      response = await query;
+      console.log('Iam here ', response);
     }
     const json = await response['_snapshot'].docChanges;
     console.log(json)
     this.setState({stories: json})
+  }
+
+  filter = (e) => {
+
+    let text = document
+      .getElementById('search')
+      .value;
+    console.log(text);
+
+    this.fetchStories(text);
+    e.preventDefault();
   }
   render() {
     if (this.state.stories === null) {
@@ -40,6 +67,16 @@ export default class StoriesList extends React.Component {
     }
     return (
       <div>
+
+        <Form inline className='text-center' onSubmit={this.filter}>
+          <div className='mx-auto'>
+          {inputs.map(input => <Grid item xs={4}>
+            <TitleInput id={input.id} label={input.label}></TitleInput>
+          </Grid>)}
+            <FormControl type="text" id='search' placeholder="Search" className=" mr-sm-2"/>
+            <Button type="submit">Search</Button>
+          </div>
+        </Form>
         <StoriesComponent
           stories={this.state.stories}
           updateFavoritesCount={this.updateFavoritesCount}></StoriesComponent>
